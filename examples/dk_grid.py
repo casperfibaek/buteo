999c22c220be7b662f5f681768f42395f4445039
casperfibaek <casperfibaek@gmailcom>
2020-06-05 10:21:56 +0100

    error handliing dk_grid

--- examples/dk_grid.py
+++ examples/dk_grid.py
@@ -1,5 +1,6 @@
 import geopandas as gpd
 import sys
+import os
 import time
 from urllib import request
 
@@ -38,21 +39,42 @@ def find_tile_names(path_to_geom):
 
     return data
 
+# Errors
+# 627_59
+# 628_59
+# 621_60
+# 627_60
 
 def download_dtm(tile_names, dst_folder, username, password):
+    completed = 0
     for tile in tile_names:
         base_path = f'ftp://{username}:{password}@ftp.kortforsyningen.dk/dhm_danmarks_hoejdemodel/DTM/'
         file_name = f'DTM_{tile.split("_", 1)[1]}_TIF_UTM32-ETRS89.zip'
-        get_file(base_path + file_name, dst_folder + file_name)
+        
+        if not os.path.exists(dst_folder + file_name):
+            try:
+                get_file(base_path + file_name, dst_folder + file_name)
+            except:
+                print(f"Error while trying to download: {base_path + file_name}")
+        completed += 1
+        print(f"Completed: {completed}/{len(tile_names)} DTM tiles.")
 
 
 def download_dsm(tile_names, dst_folder, username, password):
+    completed = 0
     for tile in tile_names:
         base_path = f'ftp://{username}:{password}@ftp.kortforsyningen.dk/dhm_danmarks_hoejdemodel/DSM/'
         file_name = f'DSM_{tile.split("_", 1)[1]}_TIF_UTM32-ETRS89.zip'
-        get_file(base_path + file_name, dst_folder + file_name)
+        
+        if not os.path.exists(dst_folder + file_name):
+            try:
+                get_file(base_path + file_name, dst_folder + file_name)
+            except:
+                print(f"Error while trying to download: {base_path + file_name}")
 
+        completed += 1
+        print(f"Completed: {completed}/{len(tile_names)} DSM tiles.")
 
 if __name__ == "__main__":
-    # download_dtm(find_tile_names('../geometry/studyAreaHull.gpkg'), '/home/cfi/data/dtm/', 'casperfibaek', 'Goldfish12')
+    download_dtm(find_tile_names('../geometry/studyAreaHull.gpkg'), '/home/cfi/data/dtm/', 'casperfibaek', 'Goldfish12')
     download_dsm(find_tile_names('../geometry/studyAreaHull.gpkg'), '/home/cfi/data/dsm/', 'casperfibaek', 'Goldfish12')
