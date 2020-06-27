15ac6ffe024a80d65d0011eb7769e4e78d0e4eed
casperfibaek <casperfibaek@gmail.com>
2020-06-27 22:35:43 +0100

    regression tests

--- paper2/ml_utils.py
+++ paper2/ml_utils.py
@@ -1,4 +1,7 @@
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
 import numpy as np
+
 
 def y_class(s):
     if s == "fid":
@@ -32,16 +35,115 @@ def minority_class_mask(arr, minority):
 def get_shape(numpy_arr):
     return (numpy_arr.shape[1], numpy_arr.shape[2], numpy_arr.shape[3])
 
-def add_rotations(X):
-    return np.concatenate([
-        np.rot90(X, k=1, axes=(1, 2)),
-        np.rot90(X, k=2, axes=(1, 2)),
-        np.rot90(X, k=3, axes=(1, 2)),
-        X,
-    ])
+def add_rotations(X, k=4):
+    if k == 1:
+        return X
+    elif k == 2:
+        return np.concatenate([
+            X,
+            np.rot90(X, k=2, axes=(1, 2)),
+        ])
+    elif k == 3:
+        return np.concatenate([
+            X,
+            np.rot90(X, k=1, axes=(1, 2)),
+            np.rot90(X, k=2, axes=(1, 2)),
+        ])
+    else:
+        return np.concatenate([
+            np.rot90(X, k=1, axes=(1, 2)),
+            np.rot90(X, k=2, axes=(1, 2)),
+            np.rot90(X, k=3, axes=(1, 2)),
+            X,
+        ])
+
 
 def add_noise(X, amount=0.01):
     return X * np.random.normal(1, amount, X.shape)
 
+
 def scale_to_01(arr):
       return (arr - arr.min()) / (arr.max() - arr.min())
+
+
+def train_split_mask(y, split=0.3, stratified=True):
+    if stratified is True:
+        strats = np.digitize(y, np.percentile(y, [10, 20, 30, 40, 50, 60, 70, 80, 90]))
+        indices = np.arange(0, len(strats), 1)
+
+        X_train, X_test, y_train, y_test = train_test_split(indices, strats, stratify=strats, test_size=split)
+
+        return (y_train, y_test)
+
+    split_amount = int(round(len(y) * split))
+
+    positive = np.full(len(y) - split_amount, True)
+    negative = np.full(split_amount, False)
+    merged = np.append(positive, negative)
+    np.random.shuffle(merged)
+
+    return (merged, ~merged)
+
+
+def add_randomness(arr):
+    flips = int(round(len(arr) / 4))
+    return np.concatenate([
+        arr[0:flips],
+        np.rot90(arr[flips:2 * flips], k=1, axes=(1, 2)),
+        np.rot90(arr[flips * 2: flips * 3], k=2, axes=(1, 2)),
+        np.rot90(arr[flips * 3:], k=3, axes=(1, 2)),
+    ])
+
+
+def viz(X, y, model, target='area'):
+    truth = y.astype("float32")
+    predicted = model.predict(X).squeeze().astype("float32")
+
+    if target == "area":
+        labels = [*range(140, 5740, 140)]
+    else:
+        labels = [*range(500, 20500, 500)]
+
+    truth_labels = np.digitize(truth, labels, right=True)
+    predicted_labels = np.digitize(predicted, labels, right=True)
+    labels_unique = np.unique(truth_labels)
+
+    residuals = (truth - predicted).astype('float32')
+    residuals = residuals / 140 if target == 'area' else residuals / 700
+
+    fig1, ax = plt.subplots()
+    ax.set_title('violin area')
+
+    per_class = []
+    for cl in labels_unique:
+        per_class.append(residuals[truth_labels == cl])
+
+    ax.violinplot(per_class, showextrema=False, showmedians=True)
+
+    plt.show()
+
+
+def histogram_selection(y, outliers=(0.1, 0.9), cut_limit=0.5, resolution=9):
+    low = np.quantile(y, outliers[0])
+    high = np.quantile(y, outliers[1])
+    step_size = (high - low) / resolution
+
+    classes = np.digitize(y, np.arange(step_size, high, step_size))
+    frequency = count_freq(classes)
+    minority = frequency.min(axis=0)[1]
+
+    min_size = int(round((len(y) * cut_limit) / resolution))
+
+    samples = []
+
+    for l in np.unique(classes):
+        count = len(classes[classes == l])
+        if minority < min_size:
+            if min_size > count:
+                samples.append(np.random.choice(np.where(classes == l)[0], count, replace=False))
+            else:
+                samples.append(np.random.choice(np.where(classes == l)[0], min_size, replace=False))
+        else:
+            samples.append(np.random.choice(np.where(classes == l)[0], minority, replace=False))
+
+    return np.hstack(samples)
--- paper2/regression.py
+++ paper2/regression.py
@@ -1,42 +1,37 @@
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.model_selection import KFold, StratifiedKFold
+from sklearn.model_selection import KFold, StratifiedKFold, train_test_split
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+import tensorflow as tf
+import tensorflow_probability as tfp
 from tensorflow.keras import Sequential, Model
 from tensorflow.keras.constraints import max_norm
-from tensorflow.keras.layers import Dense, Dropout, Conv2D, MaxPooling2D, GlobalAveragePooling2D, Flatten, BatchNormalization, Concatenate, Input
+from tensorflow.keras.layers import Dense, Dropout, Conv2D, MaxPooling2D, GlobalAveragePooling2D, Flatten, BatchNormalization, Concatenate, Input, SeparableConv2D
 from tensorflow.keras.callbacks import EarlyStopping, LearningRateScheduler, ReduceLROnPlateau, ModelCheckpoint
+from tensorflow.keras.mixed_precision import experimental as mixed_precision
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.metrics import BinaryAccuracy
 from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 import os
 import ml_utils
 import numpy as np
 
-os.environ['TF_FORCE_GPU_ALLOW_GROWTH'] = 'true'
+os.environ["TF_FORCE_GPU_ALLOW_GROWTH"] = "true"
 
 folder = "C:\\Users\\caspe\\Desktop\\Paper_2_StruturalDensity\\analysis\\"
 size = 160
+start_lr = 0.01
+epochs = 100
+batches = 64
 seed = 42
-kfolds = 5
-batches = 96
 validation_split = 0.3
 rotation = False
 noise = False
 test = False
-fold_split = True
-test_size = 10000
-noise_amount = 0.01
-learning_rate = 0.01
+test_size = 50000
+inserted_zeros = 1 # * minority class
+target = "area"
 msg = f"{str(size)} - rgbn + backscatter + coherence"
-
-def learning_rate_decay(epoch):
-  if epoch < 5:
-    return 1e-3
-  elif epoch >= 5 and epoch < 10:
-    return 1e-4
-  else:
-    return 1e-5
 
 # ***********************************************************************
 #                   LOADING DATA
@@ -45,282 +40,197 @@ def learning_rate_decay(epoch):
 blue = 0
 green = 1
 red = 2
-nir = 0
+infra = 0
 
 # Load and scale RGB channels
-X_rgb = np.load(folder + f"{str(int(size))}_rgb.npy").astype('float32')
-X_rgb[:, :, :, blue] = ml_utils.scale_to_01(np.clip(X_rgb[:, :, :, blue], 0, 4000))
-X_rgb[:, :, :, green] = ml_utils.scale_to_01(np.clip(X_rgb[:, :, :, green], 0, 5000))
-X_rgb[:, :, :, red] = ml_utils.scale_to_01(np.clip(X_rgb[:, :, :, red], 0, 6000))
+rgb = np.load(folder + f"{str(int(size))}_rgb.npy").astype("float32")
+rgb[:, :, :, blue] = ml_utils.scale_to_01(np.clip(rgb[:, :, :, blue], 0, 4000))
+rgb[:, :, :, green] = ml_utils.scale_to_01(np.clip(rgb[:, :, :, green], 0, 5000))
+rgb[:, :, :, red] = ml_utils.scale_to_01(np.clip(rgb[:, :, :, red], 0, 6000))
 
 # Load and scale NIR channel (Add additional axis to match RGB)
-X_nir = np.load(folder + f"{str(int(size))}_nir.npy").astype('float32')
-X_nir = X_nir[:, :, :, np.newaxis]
-X_nir[:, :, :, nir] = ml_utils.scale_to_01(np.clip(X_nir[:, :, :, nir], 0, 11000))
+nir = np.load(folder + f"{str(int(size))}_nir.npy").astype("float32")
+nir = nir[:, :, :, np.newaxis]
+nir[:, :, :, infra] = ml_utils.scale_to_01(np.clip(nir[:, :, :, infra], 0, 11000))
 
-# Merge RGB and NIR
-X = np.concatenate([X_rgb, X_nir], axis=3)
+# # Load Backscatter (asc + desc), clip the largest outliers (> 92%)
+bs_asc = np.load(folder + f"{str(int(size))}_bs.npy")[:, :, :, [ml_utils.sar_class("asc")]]
+bs_asc = ml_utils.scale_to_01(np.clip(bs_asc, 0, np.quantile(bs_asc, 0.98)))
+bs_desc = np.load(folder + f"{str(int(size))}_bs.npy")[:, :, :, [ml_utils.sar_class("desc")]]
+bs_desc = ml_utils.scale_to_01(np.clip(bs_desc, 0, np.quantile(bs_desc, 0.98)))
 
-# Load Backscatter (asc + desc), clip the largest outliers (> 99%)
-bs = np.load(folder + f"{str(int(size))}_bs.npy")[:, :, :, [ml_utils.sar_class("asc"), ml_utils.sar_class("desc")]]
-bs = ml_utils.scale_to_01(np.clip(bs, 0, np.quantile(bs, 0.99)))
-bs = np.concatenate([
-    bs.mean(axis=(1,2)),
-    bs.std(axis=(1,2)),
-    bs.min(axis=(1,2)),
-    bs.max(axis=(1,2)),
-    np.median(bs, axis=(1,2)),
-], axis=1)
+# # Load coherence
+# coh_asc = np.load(folder + f"{str(int(size))}_coh.npy")[:, :, :, [ml_utils.sar_class("asc")]]
+# coh_asc = ml_utils.scale_to_01(coh_asc)
+# coh_desc = np.load(folder + f"{str(int(size))}_coh.npy")[:, :, :, [ml_utils.sar_class("desc")]]
+# coh_desc = ml_utils.scale_to_01(coh_desc)
 
-# Load coherence
-coh = np.load(folder + f"{str(int(size))}_coh.npy")[:, :, :, [ml_utils.sar_class("asc"), ml_utils.sar_class("desc")]]
-coh = np.concatenate([
-    coh.mean(axis=(1,2)),
-    coh.std(axis=(1,2)),
-    coh.min(axis=(1,2)),
-    coh.max(axis=(1,2)),
-    np.median(coh, axis=(1,2)),
-], axis=1)
+X = np.concatenate([
+    rgb,
+    nir,
+    bs_asc,
+    bs_desc,
+    # coh_asc,
+    # coh_desc,
+], axis=3)
 
-sar = np.concatenate([bs, coh], axis=1)
+if target == "area":
+    labels = [*range(140, 5740, 140)]
+else:
+    labels = [*range(500, 20500, 500)]
 
-labels = [*range(140, 5740, 140)]
-y = np.load(folder + f"{str(int(size))}_y.npy")[:, ml_utils.y_class("area")]
+y = np.load(folder + f"{str(int(size))}_y.npy")[:, ml_utils.y_class(target)]
 y = (size * size) * y # Small house (100m2 * 4m avg. height)
-
-og_y = y
-og_X = X
-og_sar = sar
-
-X_rgb = None
-X_nir = None
-bs = None
-coh = None
 
 # ***********************************************************************
 #                   PREPARING DATA
 # ***********************************************************************
 
-# Subsample the areas with zero houses (vast majority)
-zero_house = y == 0
-zero_house_y = y[zero_house]
-zero_house_X = X[zero_house]
-zero_house_sar = sar[zero_house]
+# # Subsample the areas with zero houses (vast majority)
+# zero_house = y == 0
+# zero_house_y = y[zero_house]
+# zero_house_X = X[zero_house]
 
-shuffle = np.random.permutation(len(zero_house_y))
-zero_house_y = y[shuffle]
-zero_house_X = X[shuffle]
-zero_house_sar = sar[shuffle]
+# # Remove zero values from origial sample
+# y = y[~zero_house]
+# X = X[~zero_house]
 
-# Remove the top 5 percentile
-outlier_mask = np.logical_and(y > 0, y < np.quantile(y[y > 0], 0.95))
+# Balance dataset
+histogram_mask = ml_utils.histogram_selection(y)
+y = y[histogram_mask]
+X = X[histogram_mask]
 
-y = y[outlier_mask]
-X = X[outlier_mask]
-sar = sar[outlier_mask]
+# # Shuffle zero_house sample before we can add back in
+# shuffle = np.random.permutation(len(zero_house_y))
+# zero_house_y = zero_house_y[shuffle]
+# zero_house_X = zero_house_X[shuffle]
 
-# Find minority class
-class_mask = np.digitize(y, np.percentile(y, [10, 20, 30, 40, 50, 60, 70, 80, 90]))
-frequency = ml_utils.count_freq(class_mask)
-minority = frequency.min(axis=0)[1]
-
-# Undersample
-undersample_mask = ml_utils.minority_class_mask(class_mask, minority)
-y = y[undersample_mask]
-X = X[undersample_mask]
-sar = sar[undersample_mask]
-class_mask = class_mask[undersample_mask]
-
-# Add the subsampled houses back in
-y = np.concatenate([y, zero_house_y[0:minority * 2]])
-X = np.concatenate([X, zero_house_X[0:minority * 2]])
-sar = np.concatenate([sar, zero_house_sar[0:minority * 2]])
-class_mask = np.concatenate([
-    class_mask,
-    np.full(zero_house_y[0:minority * 2].shape, 99, dtype="int64"), # class 99 for the added "zero house" tiles
-])
-
-if test is True:
-    y = y[:test_size]
-    X = X[:test_size]
-    sar = sar[:test_size]
-    class_mask = class_mask[:test_size]
-
-# Rotate and add all images, add random noise to images to reduce overfit.
-if rotation is True:
-    y = np.concatenate([y, y, y, y])
-    X = ml_utils.add_rotations(X)
-    sar = ml_utils.add_rotations(sar)
-    class_mask = np.concatenate([class_mask, class_mask, class_mask, class_mask])
-
-if noise is True:
-    X = ml_utils.add_noise(X, noise_amount)
+# # Add the subsampled houses back in
+# house_samples = int(round((len(y) * 0.2) * inserted_zeros))
+# y = np.concatenate([y, zero_house_y[0:house_samples]])
+# X = np.concatenate([X, zero_house_X[0:house_samples]])
 
 # Shuffle
 shuffle = np.random.permutation(len(y))
 y = y[shuffle]
 X = X[shuffle]
-sar = sar[shuffle]
-class_mask = class_mask[shuffle]
 
+if test is True:
+    y = y[:test_size]
+    X = X[:test_size]
+
+zero_house = None
+zero_house_y = None
+zero_house_X = None
 undersample_mask = None
 shuffle = None
+
+# Randomly rotate images
+# X = ml_utils.add_randomness(X)
 
 # ***********************************************************************
 #                   ANALYSIS
 # ***********************************************************************
 
-if size == 80:
-    kernel_start = (3, 3)
-    kernel_mid = (2, 2)
-    kernel_end = (2, 2)
-elif size == 160:
-    kernel_start = (5, 5)
-    kernel_mid = (5, 5)
-    kernel_end = (3, 3)
-else:
-    kernel_start = (7, 7)
-    kernel_mid = (5, 5)
-    kernel_end = (3, 3)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
 
+model_input = Input(shape=ml_utils.get_shape(X_train), name="input")
+model = SeparableConv2D(256, kernel_size=(5, 5), padding="valid", activation="swish", kernel_initializer="he_normal")(model_input)
+model = SeparableConv2D(256, kernel_size=(3, 3), padding="same", activation="swish", kernel_initializer="he_normal")(model)
+model = MaxPooling2D(pool_size=(2, 2))(model)
+model = BatchNormalization()(model)
 
-def create_mlp_model(shape, name):
-    model_input = Input(shape=shape, name=name)
-    model = Dense(512, activation='swish', kernel_initializer='he_normal', kernel_constraint=max_norm(3))(model_input)
-    model = BatchNormalization()(model)
-    model = Dense(256, activation='swish', kernel_initializer='he_normal', kernel_constraint=max_norm(3))(model)
-    model = BatchNormalization()(model)
-    model = Dense(128, activation='swish', kernel_initializer='he_normal', kernel_constraint=max_norm(3))(model)
-    model = BatchNormalization()(model)
+# model = SeparableConv2D(512, kernel_size=(3, 3), padding="valid", activation="swish", kernel_initializer="he_normal", kernel_constraint=max_norm(3))(model)
+# model = SeparableConv2D(512, kernel_size=(2, 2), padding="same", activation="swish", kernel_initializer="he_normal", kernel_constraint=max_norm(3))(model)
+# model = MaxPooling2D(pool_size=(2, 2))(model)
+# model = BatchNormalization()(model)
 
-    return (model, model_input)
+model = SeparableConv2D(512, kernel_size=(3, 3), padding="valid", activation="swish", kernel_initializer="he_normal")(model)
+model = SeparableConv2D(512, kernel_size=(2, 2), padding="same", activation="swish", kernel_initializer="he_normal")(model)
+model = GlobalAveragePooling2D()(model)
+model = BatchNormalization()(model)
 
+model = Flatten()(model)
 
-def create_cnn_model(shape, name):
-    model_input = Input(shape=shape, name=name)
-    model = Conv2D(64, kernel_size=kernel_start, padding='same', activation='swish', kernel_initializer='he_uniform', kernel_constraint=max_norm(3))(model_input)
-    model = Conv2D(64, kernel_size=kernel_start, padding='same', activation='swish', kernel_initializer='he_uniform', kernel_constraint=max_norm(3))(model)
-    model = MaxPooling2D(pool_size=(2, 2))(model)
-    model = BatchNormalization()(model)
+model = Dense(512, activation="swish", kernel_initializer="he_normal")(model)
+model = BatchNormalization()(model)
 
-    model = Conv2D(128, kernel_size=kernel_mid, padding='same', activation='swish', kernel_initializer='he_uniform', kernel_constraint=max_norm(3))(model)
-    model = Conv2D(128, kernel_size=kernel_mid, padding='same', activation='swish', kernel_initializer='he_uniform', kernel_constraint=max_norm(3))(model)
-    model = MaxPooling2D(pool_size=(2, 2))(model)
-    model = BatchNormalization()(model)
+predictions = Dense(1, activation="relu", dtype="float32")(model)
 
-    model = Conv2D(256, kernel_size=kernel_end, padding='same', activation='swish', kernel_initializer='he_uniform', kernel_constraint=max_norm(3))(model)
-    model = Conv2D(256, kernel_size=kernel_end, padding='same', activation='swish', kernel_initializer='he_uniform', kernel_constraint=max_norm(3))(model)
-    model = GlobalAveragePooling2D()(model)
-    model = BatchNormalization()(model)
+model = Model(inputs=[model_input], outputs=predictions)
 
-    model = Flatten()(model)
+def median_error(y_actual, y_pred):
+    return tfp.stats.percentile(tf.math.abs(y_actual - y_pred), 50.0)
 
-    model = Dense(512, activation='swish', kernel_initializer='he_uniform', kernel_constraint=max_norm(3))(model)
-    model = BatchNormalization()(model)
+def median_loss(y_actual, y_pred):
+    return tf.math.square(tfp.stats.percentile(tf.math.abs(y_actual - y_pred), 50.0))
 
-    return (model, model_input)
-
-# Are we going to ensure balanced regression sets?
-kf = KFold(n_splits=kfolds)
-skf = StratifiedKFold(n_splits=kfolds)
-
-if fold_split is True:
-    splits = skf.split(np.zeros(len(class_mask)), class_mask)
-else:
-    splits = kf.split(np.zeros(len(y)), y)
-
-split = 0
-scores_mae = []
-scores_mse = []
-
-for train_index, test_index in splits:
-    X_train_1, X_test_1 = X[train_index], X[test_index]
-    X_train_2, X_test_2 = sar[train_index], sar[test_index]
-
-    y_train, y_test = y[train_index], y[test_index]
-
-    model_graph_1, input_graph_1 = create_cnn_model(ml_utils.get_shape(X_train_1), "sentinel_2")
-    model_graph_2, input_graph_2 = create_mlp_model((X_train_2.shape[1],), "sentinel_1")
-
-    model = Concatenate()([
-        model_graph_1,
-        model_graph_2,
+model.compile(
+    optimizer=Adam(
+        learning_rate=start_lr,
+        name="Adam",
+    ),
+    # loss="mean_squared_error",
+    loss="mean_absolute_error",
+    metrics=[
+        "mean_absolute_error",
+        median_error,
     ])
 
-    model = Dense(512, activation='swish', kernel_initializer='he_uniform')(model)
-    model = BatchNormalization()(model)
-    model = Dropout(0.25)(model)
+model.fit(
+    x=X_train,
+    y=y_train,
+    epochs=epochs,
+    verbose=1,
+    batch_size=batches,
+    validation_split=validation_split,
+    callbacks=[
+        EarlyStopping(
+            monitor="val_loss",
+            patience=10,
+            min_delta=1.0,
+            restore_best_weights=True,
+        ),
+        ReduceLROnPlateau(
+            monitor="val_loss",
+            factor=0.5,
+            patience=3,
+            cooldown=1,
+            min_delta=1.0,
+            min_lr=0.00001,
+        ),
+    ]
+)
 
-    predictions = Dense(1)(model)
+loss, mean_absolute_error, median_absolute_error = model.evaluate(X_test, y_test, verbose=1)
 
-    model = Model(inputs=[
-        input_graph_1,
-        input_graph_2,
-    ], outputs=predictions)
+print("Test accuracy:")
+print(f"Mean Absolute Error (MAE): {str(round(mean_absolute_error, 5))}")
+print(f"Median Absolute Error (MSE): {str(round(median_absolute_error, 5))}")
 
-    model.compile(optimizer=Adam(learning_rate=learning_rate, name='Adam'), loss='mean_absolute_error', metrics=['mean_absolute_error', 'mean_squared_error'])
+from playsound import playsound; playsound(folder + "alarm.wav", block=False)
 
-    model.fit(
-        x=[
-            X_train_1,
-            X_train_2,
-        ],
-        y=y_train,
-        epochs=500,
-        verbose=1,
-        batch_size=batches,
-        validation_split=validation_split,
-        callbacks=[
-            EarlyStopping(
-                monitor='val_loss',
-                patience=10,
-                min_delta=0.01,
-                restore_best_weights=True,
-            ),
-            ReduceLROnPlateau(
-                monitor='val_loss',
-                factor=0.1,
-                patience=5,
-                min_lr=0.00001,
-            ),
-            ModelCheckpoint(
-                f'./models/regression_{str(split)}.hdf5',
-                save_best_only=True,
-                monitor='val_loss',
-                mode='min',
-            )
-        ]
-    )
+import matplotlib.pyplot as plt
 
-    loss, mean_absolute_error, mean_squared_error = model.evaluate([X_test_1, X_test_2], y_test, verbose=1)
+truth = y.astype("float32")
 
-    print("Test accuracy:")
-    print(f"Mean Absolute Error (MAE): {str(round(mean_absolute_error, 5))}")
-    print(f"Mean Squared Error (MSE): {str(round(mean_squared_error, 5))}")
+predicted = model.predict(X).squeeze().astype("float32")
 
-    scores_mae.append(mean_absolute_error)
-    scores_mse.append(mean_squared_error)
+truth_labels = np.digitize(truth, labels, right=True)
+predicted_labels = np.digitize(predicted, labels, right=True)
+labels_unique = np.unique(truth_labels)
 
-    split += 1
+residuals = (truth - predicted).astype("float32")
+residuals = residuals / 140 if target == "area" else residuals / 700
 
-mean_mae = np.array(scores_mae).mean()
-std_mae = np.array(scores_mae).std()
-mean_mse = np.array(scores_mse).mean()
-std_mse = np.array(scores_mse).std()
+fig1, ax = plt.subplots()
+ax.set_title("violin area")
 
-print("Final regression:")
-print(f"Mean Absolute Error: {str(round(mean_mae, 5))}, stdev: {str(round(std_mae, 5))}")
-print(f"Mean Squared Error: {str(round(mean_mse, 5))}, stdev: {str(round(std_mse, 5))}")
+per_class = []
+for cl in labels_unique: per_class.append(residuals[truth_labels == cl])
 
-best_model = load_model(f"./models/regression_{str(np.argmin(np.array(scores_mae)))}.hdf5")
+ax.violinplot(per_class, showextrema=False, showmedians=True)
 
-y_labels = np.digitize(og_y, labels)
-y_pred = np.digitize(best_model.predict([og_X, og_sar]), labels)
+plt.show()
 
-print(confusion_matrix(y_labels, y_pred))
-print(classification_report(y_labels, y_pred))
-print(accuracy_score(y_labels, y_pred))
-
-np.savetxt("./models/confusion_matrix_160_area_140_interval.csv", confusion_matrix(y_labels, y_pred), delimiter=",") 
-
-from playsound import playsound; playsound(folder + "alarm.wav")
+import pdb; pdb.set_trace()
