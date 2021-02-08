9cfbc3f36375ed78a164ec8bb1130dd0583a74da
casperfibaek <casperfibaek@gmail.com>
2021-02-08 14:48:11 +0100

    cleaning

--- test.py
+++
@@ -1,4 +0,0 @@
-names = ['casper', 'torben', 'sofie', 'millie']
-
-for name in names:
-  import pdb; pdb.set_trace()
--- tests/base_tests.py
+++
@@ -1,3 +0,0 @@
-import sys; sys.path.append('..')
-from lib.raster_io import raster_to_array, array_to_raster
-
