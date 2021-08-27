6a4f3f231050da5f2426cc34c4c95f7a51d2d8ef
casperfibaek <casperfibaek@gmail.com>
2021-08-28 00:49:37 +0200

    big_model_training

--- papers/structure_estimates/model_trio_down.py
+++ papers/structure_estimates/model_trio_down.py
@@ -12,7 +12,7 @@ def model_trio_down(
     shape_lower,
     activation="relu",
     kernel_initializer="glorot_normal",
-    sizes=[48, 64, 80],  # big_model
+    sizes=[40, 48, 56],  # big_model
     # sizes=[32, 40, 48], # small model
     inception_blocks=2,
     name="denmark",
