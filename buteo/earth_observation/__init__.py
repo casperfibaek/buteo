bd809ad2bbed022636dfef3729c63af6d596867d
casperfibaek <casperfibaek@gmail.com>
2022-01-27 15:56:19 +0100

    docstring update test.

---
+++ buteo/earth_observation/__init__.py
@@ -0,0 +1,7 @@
+"""
+Earth Observation toolbox for Python. Contains functions for:
+Sentinel 1 & 2. Preprocessing, downloading and mosaic.
+
+Textures can be generated through the filters module.
+
+"""
