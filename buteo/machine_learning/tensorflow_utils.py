72e258819332c65a2ed684d45a0cb51cc3ac500c
casperfibaek <casperfibaek@gmail.com>
2022-02-04 10:20:52 +0100

    LearningRateAdjuster

--- buteo/machine_learning/tensorflow_utils.py
+++ buteo/machine_learning/tensorflow_utils.py
@@ -30,7 +30,6 @@ def mse_mae_mix_loss(y_true, y_pred):
     return tf.math.multiply(mse, mae)
 
 
-
 def mish(inputs):
     return inputs * tf.math.tanh(tf.math.softplus(inputs))
 
@@ -142,3 +141,45 @@ class OverfitProtection(tf.keras.callbacks.Callback):
 
             if self.verbose:
                 print(f"Training stopped to prevent overfitting. Difference: {ratio}, Patience: {self.count}/{self.patience}")
+
+
+class LearningRateAdjuster(tf.keras.callbacks.Callback):
+    def __init__(self, start_epoch, decay_rate=0.95, decay_rate_epoch=10, set_at_end="end_rate", step_wise=True, verbose=True):
+        self.start_epoch = start_epoch
+        self.decay_rate = decay_rate
+        self.decay_rate_epoch = decay_rate_epoch
+        self.set_at_end = set_at_end
+        self.step_wise = step_wise
+        self.initial_epoch = 0
+        self.decay_count = 0
+        self.verbose = verbose
+        self.initial_lr = None
+
+    def on_epoch_end(self, epoch, logs=None):
+        old_lr = self.model.optimizer.lr.read_value()
+        new_lr = old_lr * self.decay_rate ^ ((epoch - self.start_epoch) / self.decay_rate_epoch)
+
+        if self.step_wise and epoch == (self.start_epoch + (self.decay_rate_epoch * (self.decay_count + 1))):
+            if self.verbose:
+                print(f"\nEpoch: {epoch}. Reducing Learning Rate from {old_lr} to {new_lr}")
+
+            self.model.optimizer.lr.assign(new_lr)
+            self.decay_count += 1
+
+        elif not self.step_wise:
+            if self.verbose:
+                print(f"\nEpoch: {epoch}. Reducing Learning Rate from {old_lr} to {new_lr}")
+
+            self.model.optimizer.lr.assign(new_lr)
+    
+    def on_epoch_begin(self, epoch, logs=None):
+        if self.initial_lr is None:
+            self.initial_lr = self.model.optimizer.lr.read_value()
+
+    def on_train_end(self, logs=None):
+        if self.set_at_end == "end_rate":
+            pass
+        elif isinstance(self.set_at_end, (int, float)):
+            self.model.optimizer.lr.assign(self.set_at_end)
+        elif self.set_at_end == "initial":
+            self.model.optimizer.lr.assign(self.initial_lr)
