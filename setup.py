dbf02dc7f1f2bee9a6ee8e8e3fefacf3333a877f
casperfibaek <casperfibaek@gmail.com>
2022-07-06 14:47:49 +0200

    build process 2

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def readme():
 
 setuptools.setup(
     name="Buteo",
-    version="0.5.0",
+    version="0.5.2",
     author="Casper Fibaek",
     author_email="casperfibaek@gmail.com",
     description="A pythonic way of working with raster data",
