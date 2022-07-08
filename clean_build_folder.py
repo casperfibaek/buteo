4bc0acd958948d21c071293d72401c88351fa6b8
casperfibaek <casperfibaek@gmail.com>
2022-07-08 11:25:45 +0200

    clean script works

--- clean_build_folder.py
+++ clean_build_folder.py
@@ -1,7 +1,10 @@
 import os
 from glob import glob
 
-builds_glob = "~/anaconda3/conda-bld/*.tar.bz2"
+home = os.path.expanduser("~")
+builds_folder = os.path.abspath(home + "/anaconda3/conda-bld/")
+builds_glob = os.path.join(builds_folder, "**/*.tar.bz2")
+
 
 for build in glob(builds_glob):
     os.remove(build)
