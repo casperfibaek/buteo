815c950e860a228672ccb0542a301e2b2349905a
casperfibaek <casperfibaek@gmail.com>
2022-09-29 16:41:25 +0200

    psutil version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 """ Build script for pip and conda package. """
 from setuptools import setup, find_packages
 
-VERSION = "0.7.48"
+VERSION = "0.7.49"
 
 def readme():
     """ Generate readme file. """
