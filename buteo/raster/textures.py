862ee20478517727cef0d7b507fc6b3f5fc12e78
casperfibaek <casperfibaek@gmail.com>
2022-09-30 11:23:22 +0200

    nodata for textures

--- buteo/raster/textures.py
+++ buteo/raster/textures.py
@@ -9,32 +9,32 @@ import sys; sys.path.append("../../")
 from buteo.raster.convolution import convolve_array, get_kernel
 
 
-def texture_local_variance(img, filter_size=5, spherical=False):
+def texture_local_variance(img, filter_size=5, spherical=False, nodata=False, nodata_value=9999.0):
     """ Create a variance texture layer"""
     _kernel, weights, offsets = get_kernel(filter_size, distance_weight="linear", spherical=spherical)
-    std = convolve_array(img, offsets, weights, "std")
+    std = convolve_array(img, offsets, weights, "std", nodata=nodata, nodata_value=nodata_value)
 
     return std
 
 
-def texture_local_median(img, filter_size=5, spherical=False):
+def texture_local_median(img, filter_size=5, spherical=False, nodata=False, nodata_value=9999.0):
     """ Create a variance texture layer"""
     _kernel, weights, offsets = get_kernel(filter_size, distance_weight="linear", spherical=spherical)
-    median = convolve_array(img, offsets, weights, "median")
+    median = convolve_array(img, offsets, weights, "median", nodata=nodata, nodata_value=nodata_value)
 
     return median
 
 
-def texture_local_blur(img, filter_size=5, spherical=False):
+def texture_local_blur(img, filter_size=5, spherical=False, nodata=False, nodata_value=9999.0):
     """ Create a variance texture layer"""
     _kernel, weights, offsets = get_kernel(filter_size, distance_weight="linear", spherical=spherical, hole=False)
-    return convolve_array(img, offsets, weights, "sum")
+    return convolve_array(img, offsets, weights, "sum", nodata=nodata, nodata_value=nodata_value)
 
 
-def texture_hole_dif(img, filter_size=5, spherical=False):
+def texture_hole_dif(img, filter_size=5, spherical=False, nodata=False, nodata_value=9999.0):
     """ Create a variance texture layer"""
     _kernel, weights, offsets = get_kernel(filter_size, distance_weight="linear", spherical=spherical, hole=True)
-    hole = convolve_array(img, offsets, weights, "sum")
+    hole = convolve_array(img, offsets, weights, "sum", nodata=nodata, nodata_value=nodata_value)
 
     hole_dif = img / hole
 
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 """ Build script for pip and conda package. """
 from setuptools import setup, find_packages
 
-VERSION = "0.7.61"
+VERSION = "0.7.62"
 
 def readme():
     """ Generate readme file. """
