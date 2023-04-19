e10c9307ae86ca25092e931e5c3e157af710e4a8
casperfibaek <casperfibaek@gmail.com>
2023-04-19 08:42:54 +0200

    change pdoc format to google

--- build_documentation.py
+++ build_documentation.py
@@ -3,7 +3,6 @@ Build the anaconda package
 """
 import os
 import shutil
-from glob import glob
 
 # Clean documentation folder
 shutil.rmtree('./docs/', ignore_errors=True)
@@ -12,4 +11,4 @@ os.mkdir("./docs/")
 os.mkdir("./docs/buteo/")
 
 # Build documentation
-os.system("pdoc ./buteo -o ./docs")
+os.system("pdoc ./buteo -o ./docs --docformat google")
