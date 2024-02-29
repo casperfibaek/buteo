cbf9f0e922bb7407e9c92067d9767d59e3268ce5
casperfibaek <casperfibaek@gmail.com>
2024-02-29 13:25:02 +0100

    lowered numba requirements

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 """ Build script for pip and conda package. """
 from setuptools import setup, find_packages
 
-VERSION = "0.9.56"
+VERSION = "0.9.57"
 
 def readme():
     """ Generate readme file. """
