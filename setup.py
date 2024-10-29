4b402bb686715d1366b664a6f19df6bb51d7809c
casperfibaek <casperfibaek@gmail.com>
2024-10-29 16:17:39 +0100

    version bump and documentation

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 """ Build script for pip and conda package. """
 from setuptools import setup, find_packages
 
-VERSION = "0.9.62"
+VERSION = "0.9.63"
 
 def readme():
     """ Generate readme file. """
